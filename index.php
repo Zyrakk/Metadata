f081344db41ba373a077ed3e690088c13bf15bb9
Zyrakk <sykblade1@gmail.com>
2024-10-01 14:27:53 +0200

    css modified

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
     <div class="container">
         <h1>Eliminador de Metadatos de Imágenes</h1>
         <form action="procesar.php" method="POST" enctype="multipart/form-data" id="uploadForm">
-            <label for="file">Sube una imagen (JPEG, PNG o GIF, máximo 5MB):</label>
+            <!-- Asignamos el 'for' con el mismo ID que el input -->
+            <label for="file" class="upload-label">Sube una imagen (JPEG, PNG o GIF, máximo 5MB):</label>
+            <!-- Input file oculto -->
             <input type="file" name="file" id="file" required>
             <button type="submit" id="submitBtn">Mostrar Metadatos</button>
         </form>
